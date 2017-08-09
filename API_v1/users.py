ae3edfee948cdb2fa746e845c287885771f431ea
root <kumar.santosh827@gmail.com>
2017-08-09 10:23:26 +0530

    changes..

--- API_v1/users.py
+++ API_v1/users.py
@@ -7,29 +7,55 @@ from passlib.hash import sha256_crypt
 
 
 client = MongoClient('localhost', 27017)  
-db = client.bis_research  
-usersTbl = db.users 
+db = client.bis_research
 
+usersTbl = db.users
+planTbl  = db.plan_master 
+
+#Post Login
 def login():
-
+	usersTbl = db.users
+	planTbl  = db.plan_master 
 	user_data = request.json
+
 	if request.method =='POST' and 'email' in user_data and 'password' in user_data :
-		if user_data['email'] or user_data['password']:
-			results = usersTbl.find({"email":user_data['email']})
-			# return dumps(results[0]['password'])
-			if results.count()==1 and sha256_crypt.verify(user_data['password'],results[0]['password']):
-			  return Response(dumps({"response": { "status": "success","message": "login success", "data":results }}), mimetype='application/json') 
+		if user_data['email'] and user_data['password']:
+
+			userLogin = usersTbl.find_one({"email":user_data['email']})
+			# return dumps(userLogin['plan_id'])
+
+			if 'password' in userLogin and sha256_crypt.verify(user_data['password'],userLogin['password']):
+				if 'plan_id' in userLogin and userLogin['plan_id']:
+					   del(userLogin['password'])
+					   userPlan = planTbl.find_one({"_id":ObjectId(userLogin['plan_id'])})
+					   userLogin['userPlan'] = userPlan
+				else :
+					   userLogin['userPlan'] = ""
+				return Response(dumps({"response": { 
+			  	                     "status": "success",
+			  	                     "message": "login success", 
+			  	                     "data":userLogin }}), mimetype='application/json') 
+			  
 			else :
-			  return Response(dumps({"response": { "status": "failed","message": "Invalid email or password","data":"[]" }}), mimetype='application/json')
+			  return Response(dumps({"response": { 
+			  	                     "status": "failed",
+			  	                     "message": "Invalid email or password",
+			  	                     "data":"" }}), mimetype='application/json')
 		else :
-				return Response(dumps({"response": {"status": "failed","message": "Missing email or password","data":"[]"  } }), mimetype='application/json')		 	             
+				return Response(dumps({"response": {
+					                     "status": "failed",
+					                     "message": "Missing email or password",
+					                     "data":""  } }), mimetype='application/json')		 	             
 	else:
-		  return Response(dumps({"response":{ "status": "failed", "message": " Missing required param", "data":"[]"  }  }), mimetype='application/json')
+		  return Response(dumps({"response":{ 
+		  	                     "status": "failed", 
+		  	                     "message": " Missing required param",
+		  	                     "data":""  }  }), mimetype='application/json')
 
 
 def pwd_hash():
 	#generate
 	password = sha256_crypt.encrypt("12345678")
-	#veryfy
+	#verify
 	#sha256_crypt.verify('123',password)
 	return dumps(password)
